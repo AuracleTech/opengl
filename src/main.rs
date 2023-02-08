b1fc60698cd61801df535ca20d2743c1a75068aa
Auracle <virgilfabre@outlook.com>
2023-02-07 19:56:16 -0500

    Force OpenGL 3.3 core profile

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,12 @@ use glfw::{Action, Context, Key};
 fn main() {
     let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).expect("Failed to initialize GLFW.");
 
+    // set opengl version to 3.3 core profile
+    glfw.window_hint(glfw::WindowHint::ContextVersion(3, 3));
+    glfw.window_hint(glfw::WindowHint::OpenGlProfile(
+        glfw::OpenGlProfileHint::Core,
+    ));
+
     let (mut window, events) = glfw
         .create_window(
             1280,
