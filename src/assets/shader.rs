2e769c30e807431e2d278ad496fe6aa920028668
Auracle <virgilfabre@outlook.com>
2023-03-28 18:40:31 -0400

    Shader Refactoring

--- src/assets/shader.rs
+++ src/assets/shader.rs
@@ -18,54 +18,22 @@ impl Shader {
         file.read_to_string(&mut source)
             .expect("Failed to read shader file.");
 
-        let id = unsafe { gl::CreateShader(shader_type) };
+        let gl_id = unsafe { gl::CreateShader(shader_type) };
 
-        if id <= 0 {
+        if gl_id <= 0 {
             panic!("The shader id is invalid.");
         }
 
         // Compile shader
         let source = CString::new(source).expect("Failed to convert source to CString.");
         unsafe {
-            gl::ShaderSource(id, 1, &source.as_ptr(), std::ptr::null());
-            gl::CompileShader(id);
+            gl::ShaderSource(gl_id, 1, &source.as_ptr(), std::ptr::null());
+            gl::CompileShader(gl_id);
         }
 
-        // Verify shader compiled
-        let mut success = 0;
-        unsafe {
-            gl::GetShaderiv(id, gl::COMPILE_STATUS, &mut success);
-        }
+        verify_shader(gl_id, extension);
 
-        if success != 0 {
-            return Self { gl_id: id };
-        }
-
-        let mut log_length = 0;
-        unsafe {
-            gl::GetShaderiv(id, gl::INFO_LOG_LENGTH, &mut log_length);
-        }
-        let mut log = Vec::with_capacity(log_length as usize);
-        unsafe {
-            gl::GetShaderInfoLog(
-                id,
-                log_length,
-                std::ptr::null_mut(),
-                log.as_mut_ptr() as *mut GLchar,
-            );
-            log.set_len(log_length as usize);
-        }
-        let why = String::from_utf8(log).expect("Failed to convert log to String.");
-        panic!(
-            "Failed to compile shader type {}: {}",
-            match shader_type {
-                gl::VERTEX_SHADER => "Vertex",
-                gl::FRAGMENT_SHADER => "Fragment",
-                gl::GEOMETRY_SHADER => "Geometry",
-                _ => "Unknown",
-            },
-            why
-        );
+        Self { gl_id }
     }
 }
 
@@ -74,5 +42,30 @@ impl Drop for Shader {
         unsafe {
             gl::DeleteShader(self.gl_id);
         }
+    }
+}
+
+fn verify_shader(gl_id: GLuint, extension: &str) {
+    let mut success = 0;
+    unsafe {
+        gl::GetShaderiv(gl_id, gl::COMPILE_STATUS, &mut success);
+    }
+    if success == 0 {
+        let mut log_length = 0;
+        unsafe {
+            gl::GetShaderiv(gl_id, gl::INFO_LOG_LENGTH, &mut log_length);
+        }
+        let mut log = Vec::with_capacity(log_length as usize);
+        unsafe {
+            gl::GetShaderInfoLog(
+                gl_id,
+                log_length,
+                std::ptr::null_mut(),
+                log.as_mut_ptr() as *mut GLchar,
+            );
+            log.set_len(log_length as usize);
+        }
+        let why = String::from_utf8(log).expect("Failed to convert log to String.");
+        panic!("Failed to compile shader extension {}: {}", extension, why);
     }
 }
