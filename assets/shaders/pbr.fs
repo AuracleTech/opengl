e7ee07eaec48c9ce5f3fc5f821fd3ea542ce39d1
Auracle <virgilfabre@outlook.com>
2023-03-28 04:46:34 -0400

    Dithering noise

--- assets/shaders/pbr.fs
+++ assets/shaders/pbr.fs
@@ -15,6 +15,10 @@ float LinearizeDepth(float depth)
     return (2.0 * near * far) / (far + near - depth_normalized * (far - near));	
 }
 
+float random_number_0_1(vec2 uv)
+{
+    return fract(sin(dot(uv.xy, vec2(12.9898, 78.233))) * 43758.5453);
+}
 
 void main()
 {
@@ -22,10 +26,14 @@ void main()
 
     vec3 norm = normalize(normal);
     vec3 diffuse = texture(albedo, tex_coord).rgb;
-    result += vec4(diffuse, 1.0) * 0.0001;
+    result += vec4(diffuse, 1.0);
 
     float depth = LinearizeDepth(gl_FragCoord.z) / far; // divide by far for demonstration
     result += vec4(vec3(depth), 1.0);
+
+    // result = result * 0.00001;
+
+    result += vec4(vec3(random_number_0_1(tex_coord) / 256), 1.0);
 
     frag_color = result;
 }
