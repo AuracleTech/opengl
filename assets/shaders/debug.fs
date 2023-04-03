b7ce639682b247939b52b31fd2c80390a87b51eb
Auracle <virgilfabre@outlook.com>
2023-04-03 01:52:58 -0400

    Kernel post effect

--- assets/shaders/debug.fs
+++ assets/shaders/debug.fs
@@ -12,17 +12,36 @@ float dither(vec2 uv)
     return fract(sin(dot(uv.xy, vec2(12.9898, 78.233))) * 43758.5453) / 256.0;
 }
 
+const float offset = 1.0 / 300.0;  
+
 void main()
 {
-    frag_color = texture(frame, tex_coord);
-    float average = (frag_color.r + frag_color.g + frag_color.b) / 3.0;
-    frag_color.rgb = vec3(average);
+    vec2 offsets[9] = vec2[](
+        vec2(-offset,  offset), // top-left
+        vec2( 0.0f,    offset), // top-center
+        vec2( offset,  offset), // top-right
+        vec2(-offset,  0.0f),   // center-left
+        vec2( 0.0f,    0.0f),   // center-center
+        vec2( offset,  0.0f),   // center-right
+        vec2(-offset, -offset), // bottom-left
+        vec2( 0.0f,   -offset), // bottom-center
+        vec2( offset, -offset)  // bottom-right    
+    );
 
-    float dither_value = dither(tex_coord);
-    if (mod(gl_FragCoord.x, 2.0) < 1.0) {
-        frag_color.rgb += dither_value;
+    float kernel[9] = float[](
+        -1, -1, -1,
+        -1,  9, -1,
+        -1, -1, -1
+    );
+    
+    vec3 sampleTex[9];
+    for(int i = 0; i < 9; i++)
+    {
+        sampleTex[i] = vec3(texture(frame, tex_coord.st + offsets[i]));
     }
-    if (mod(gl_FragCoord.y, 2.0) < 1.0) {
-        frag_color.rgb -= dither_value;
-    }
+    vec3 col = vec3(0.0);
+    for(int i = 0; i < 9; i++)
+        col += sampleTex[i] * kernel[i];
+    
+    frag_color = vec4(col, 1.0);
 }
